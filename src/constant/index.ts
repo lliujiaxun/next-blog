2de7e2df989b79bdaf6ae08671600393983d743d
xun <lliujiaxun@gmail.com>
2023-12-05 13:49:04 +0800

    ✨ feat: 修改配置

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,1 +1,1 @@
-export const API_HOST = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.API_HOST
+export const API_HOST = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''
